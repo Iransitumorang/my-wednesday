97f45ecafccf9827b2d099c392e6e1a0eb315f1f
Iran Situmorang <iran.situmorang@iconpln.co.id>
2026-06-15 13:51:44 +0700

    compose 2

--- my-wednesday/vite.config.js
+++ my-wednesday/vite.config.js
@@ -1,72 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   server: {
-    port: 3000,
+    port: 3001,
     proxy: {
-      '/auth': {
-        target: 'http://localhost:8080',
+      '/api': {
+        target: 'http://192.168.8.139:8080',
         changeOrigin: true,
-        secure: false,
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
-            if (req.headers.authorization) {
-              proxyReq.setHeader('Authorization', req.headers.authorization)
-            }
-          })
-        },
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      '/hotels': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        bypass: (req) => (req.headers.accept?.includes('text/html') ? '/index.html' : null),
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
-            if (req.headers.authorization) {
-              proxyReq.setHeader('Authorization', req.headers.authorization)
-            }
-          })
-        },
-      },
-      '/rooms': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        bypass: (req) => (req.headers.accept?.includes('text/html') ? '/index.html' : null),
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
-            if (req.headers.authorization) {
-              proxyReq.setHeader('Authorization', req.headers.authorization)
-            }
-          })
-        },
-      },
-      '/bookings': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        bypass: (req) => (req.headers.accept?.includes('text/html') ? '/index.html' : null),
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
-            if (req.headers.authorization) {
-              proxyReq.setHeader('Authorization', req.headers.authorization)
-            }
-          })
-        },
-      },
-    },
-  },
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
